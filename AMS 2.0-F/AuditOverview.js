c5c5b1c3f13d3ae4ecb6f744b4188b69ef61e4b4
Satyam Vivek <129862783+satyamvivek@users.noreply.github.com>
2023-06-28 10:03:07 +0530

    auditoverview js
    ajax part for advance searching

--- AMS 2.0-F/AuditOverview.js
+++ AMS 2.0-F/AuditOverview.js
@@ -5,6 +5,48 @@ $(document).ready(function () {
 })
 
 
+// Using Ajax to fetch data from the Node.js API
+$.ajax({
+    url: 'http://localhost:3000/advanceSearchForAudit',
+    type: 'POST',
+    dataType: 'json',
+    success: function (response) {
+        // Assuming the response is an array of objects
+        if (response && response.length > 0) {
+            // Creating the table header
+            console.log(response);
+
+        //     var table = $('<table>');
+        //     var headerRow = $('<tr>');
+        //     headerRow.append('<th>LocationId</th>');
+        //     headerRow.append('<th>DepartmentId</th>');
+        //     headerRow.append('<th>EmployeeNo</th>');
+        //     table.append(headerRow);
+
+        //     // Populating the table with data
+        //     for (var i = 0; i < response.length; i++) {
+        //         var rowData = response[i];
+        //         var dataRow = $('<tr>');
+        //         dataRow.append('<td>' + rowData.LocationId + '</td>');
+        //         dataRow.append('<td>' + rowData.DepartmentId + '</td>');
+        //         dataRow.append('<td>' + rowData.EmployeeNo + '</td>');
+        //         table.append(dataRow);
+        //     }
+
+        //     // Adding the table to the HTML page
+        //     $('#tableContainer').empty().append(table);
+        }
+        //  else {
+        //     $('#tableContainer').text('No data found.');
+        // }
+    },
+    error: function (xhr, status, error) {
+        console.error('Error:', error);
+    }
+});
+
+
+
 
 
 
