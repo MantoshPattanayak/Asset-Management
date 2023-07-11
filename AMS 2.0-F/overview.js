dd437853234a6f0629f363066259dc1b01076aa7
Soumyak Ranjan Behera <126449331+soumyak1234@users.noreply.github.com>
2023-07-11 10:51:23 +0530

    Update overview.js

--- AMS 2.0-F/overview.js
+++ AMS 2.0-F/overview.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  if(sessionStorage.getItem('sessionVar') != 'pass' && sessionStorage.getItem('sessionVar') != 'userPass'){
+  if(sessionStorage.getItem('sessionVar') != 'pass'){
     window.location.href = `./index.html`;
   }
   console.log("document ready");
